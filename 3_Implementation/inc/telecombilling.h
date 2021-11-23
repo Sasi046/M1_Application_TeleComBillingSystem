f3971d562910cdb6d34c25bdb5a0967c461d6578
Sasi046 <94299033+Sasi046@users.noreply.github.com>
2021-11-23 21:39:11 +0300

    Update telecombilling.h

--- 3_Implementation/inc/telecombilling.h
+++ 3_Implementation/inc/telecombilling.h
@@ -11,7 +11,12 @@
 #include<ctype.h>
 #include<windows.h>
 #include<stdlib.h>
-
+struct subscriber
+{
+char phonenumber[20];
+char name[50];
+float amount;
+}s;
 void addrecords();
 void listrecords();
 void modifyrecords();
