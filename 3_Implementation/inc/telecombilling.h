bc643b1f70538ccb5a01dea154adc0c3f67f7608
Sasi046 <94299033+Sasi046@users.noreply.github.com>
2021-11-23 21:33:52 +0300

    Create telecombilling.h

---
+++ 3_Implementation/inc/telecombilling.h
@@ -0,0 +1,22 @@
+/** 
+* @file telecom billing.h
+* Billing application
+*
+*/
+#ifndef __TELECOM_BILLING_H_
+#define __TELECOM_BILLING_H__
+
+#include<stdio.h>
+#include<conio.h>
+#include<ctype.h>
+#include<windows.h>
+#include<stdlib.h>
+
+void addrecords();
+void listrecords();
+void modifyrecords();
+void deleterecords();
+void searchrecords();
+void payment();
+ 
+#endif  /* #define __TELECOM_BILLING_H__ */
