e221e21de56f9cd8e7453e78d995731415b9f4d0
Sasi046 <94299033+Sasi046@users.noreply.github.com>
2021-11-25 09:35:28 +0300

    Update and rename telecombilling.h to Fun.h

---
+++ 3_Implementation/inc/Fun.h
@@ -0,0 +1,21 @@
+/** 
+* @file Fun.h
+* billing application
+*
+*/
+#ifndef _FUN_H_
+#define _FUN_H_
+
+
+
+void addrecords();
+void listrecords();
+void modifyrecords();
+void deleterecords();
+void searchrecords();
+void payment();
+
+
+
+
+#endif  /* #define _Fun_H_  */
